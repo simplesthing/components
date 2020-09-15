127ee241f4ebd6819d16abdb3f3c4e65d3e2a26c
ava <i.am.simplesthing@gmail.com>
2020-09-14 19:59:50 -0700

    add storybook

---
+++ packages/storybook/src/components/Test.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export const Test = () => {
+  return <h1>Test</h1>;
+};
+
+Test.propTypes = {};
+
+Test.defaultProps = {};
---
+++ packages/storybook/src/components/Test.stories.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { Test } from "./Test";
+
+export default {
+  title: "Test",
+  component: Test,
+};
+
+const Template = () => <Test />;
+
+export const Primary = Template.bind({});
